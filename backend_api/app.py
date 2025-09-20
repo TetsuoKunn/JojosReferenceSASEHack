bfbb6aea266dc815c14702bb18fc4a097c109df0
subbel <pkumar96@asu.edu>
2025-09-20 13:39:03 -0700

    security

---
+++ backend_api/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, abort
+from markupsafe import escape
+# from MessageDatabase import *
+# from User import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv("../")
+API_KEY = os.getenv("API_KEY")
+
+app = Flask(__name__)
+
+def security_check(key):
+    if(key != API_KEY):
+        abort(403)
+
+
+# The following is the easiest way to implement a url that performs CRUD operations
+@app.route("/")
+def hello_world():
+    key = request.headers.get("X-API-KEY") or request.args.get("key")
+    name = request.args.get("name", "unprovided name")
+    security_check(key)
+    return f"hello, {escape(name)}"
+
+#
+# SAFETY PROTOCOL NOT IMPLEMENTED YET PLS WAIT
+#
+
+# @app.route("/<appropriate_url>")
+# def <function_name>():
+#     variable = request.args.get("variable", "default")
+#     below is required to ensure some form of sofety
+#     key = request.headers.get("X-API-KEY") or request.args.get("key")
+#     security_check(key)
+#     run_username_func(variable)
+#     """ will run the function if username provided is in the url in the form of /<appropriateurl>?variable=henry"""
+#     the return function could just be a confirmation or it could be returning any appropriate data
+#     return f"hello, {escape(name)}"
+
+# Add as many variables as you need
+# last_name = request.args.get("last", "App")
+# age = request.args.get("age", "unknown")
+# here app and unknown are default values incase the user doesn't enter them

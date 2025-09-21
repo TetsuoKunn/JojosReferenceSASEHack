f5ff40bf7395014bee7bef918b7e3134b225bb8b
subbel <pkumar96@asu.edu>
2025-09-20 23:49:41 -0700

    frontend views

--- guilds/pages/views.py
+++ guilds/pages/views.py
@@ -3,3 +3,36 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request ,"index.html")
+
+def signin(request):
+    pass
+
+def register(request):
+    pass
+
+def search(request):
+    pass
+
+def profile_view(request):
+    pass
+
+def guild_view(request):
+    pass
+
+def post_view(request):
+    pass
+
+def guild_creation_view(request):
+    pass
+
+def guild_user_view(request):
+    pass
+
+def guild_user_owner_view(request):
+    pass
+
+def messaging_view(request):
+    pass
+
+def message_view(request):
+    pass

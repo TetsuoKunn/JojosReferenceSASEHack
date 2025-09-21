6014e81ae40400971c42e0657d1a2d83df2c6cb7
TetsuoKunn <cfritz3@asu.edu>
2025-09-21 10:36:21 -0700

    Functions being made

--- backend_api/MessageDatabase.py
+++ backend_api/MessageDatabase.py
@@ -39,25 +39,26 @@ class DatabaseHelper:
         self.run.execute("""
            CREATE TABLE IF NOT EXISTS Guild (
             guildID INT AUTO_INCREMENT PRIMARY KEY, 
-            name VARCHAR(MAX), 
-            userID INT, 
+            name TEXT, 
+            username VARCHAR(15), 
             creationDate DATETIME, 
             description TEXT,
             country TEXT, 
             state TEXT, 
             city TEXT, 
-            FOREIGN KEY userID REFERENCES Users(userID)
+            FOREIGN KEY username REFERENCES Users(username)
             )              
         """)
         
         # Creates the GuildUserList
         self.run.execute(""" 
            CREATE TABLE IF NOT EXISTS GuildUserList (
-            guildID INT, 
-            userID INT, 
+            name TEXT, 
+            username VARCHAR(15), 
             role VARCHAR(100), 
             datejoined DATETIME, 
-            FOREIGN KEY guildID REFERENCES Guild(guildID)
+            FOREIGN KEY name REFERENCES Guild(name), 
+            FOREIGN KEY username REFERENCES Users(username)
             )              
         """)
 
@@ -65,13 +66,13 @@ class DatabaseHelper:
         self.run.execute("""
            CREATE TABLE IF NOT EXISTS Posts (
             postID INT AUTO_INCREMENT PRIMARY KEY, 
-            userID INT, 
+            username VARCHAR(15), 
             guildID INT DEFAULT -1, 
             text TEXT, 
             pictureID INT, 
             creationDate DATETIME, 
             likes INT,
-            FOREIGN KEY (userID) REFERENCES Users(userID), 
+            FOREIGN KEY (username) REFERENCES Users(username), 
             FOREIGN KEY (guildID) REFERENCES Guild(guildID), 
             FOREIGN KEY (pictureID) REFERENCES Pictures(pictureID), 
             )             
@@ -120,95 +121,151 @@ class DatabaseHelper:
             )
         """)
 
-
-    def register(username, password, joiningDate): 
+    
+    def register(self, username, password, joiningDate): 
         """"
         Function to make a new user. 
         """
-
+        self.run.execute(f"INSERT INTO Users (username, password, joiningDate) VALUES ({username}, {password}), {joiningDate}")
+        self.mydb.commit()
         
-    def login(username, password): 
+
+    def login(self, username, password): 
         """
         Function that checks for a user's existance. 
         """
-
-
-    def updateRole(username, role): 
+        self.run.execute("SELECT 1 FROM Users WHERE username = ? AND password = ? LIMIT 1", (username, password))
+        exists = self.run.fetchone() is not None
+ 
+        return exists   # True if login is successful, False otherwise. 
+    
+    
+    def updateRole(self, username, guildName, role): 
         """
         Function to update the Role of a person.
         """
+        self.run.execute(f"UPDATE GuildUserList SET role = ? WHERE username = ? AND guildName = ?", (role, username, guildName))
+        self.mydb.commit()
 
 
-    def joinGuild(username, guildName):
+    def joinGuild(self, username, guildName, joiningDate):
         """
         Function to join a pre-existing guild.
         """
+        self.run.execute("INSERT INTO GuildUserList (name, username, role, datejoined) VALUES (?, ?,'Novice', ?)", (guildName, username, joiningDate))
+        self.mydb.commit()
 
 
-    def registerGuild(guildName, username, creationDate, description, country, state, city): 
+    def registerGuild(self, guildName, username, creationDate, description, country, state, city): 
         """
         Function to register a new guild. 
         """
+        self.run.execute("INSERT INTO Guild (guildName, username, creationDate, description, country, state, city) VALUES (?, ?, ?, ?, ?, ?, ?)", (guildName, username, creationDate, description, country, state, city))
+        self.mydb.commit()
+        self.updateRole(username, guildName, "Owner")
 
 
-    def getAllUsersInGuild(guildName): 
+    def getAllUsersInGuild(self, guildName): 
         """
-        Returns all users under a guild. 
+        Returns all users under a guild as tuples
         """
+        self.run.execute(
+            """
+            SELECT username, role, datejoined
+            FROM GuildUserList
+            WHERE guildName = ?
+            """, (guildName))
+        users = self.run.fetchall()
+
+        return users
 
 
-    def getAllGuildsCountry(country): 
+    def getAllGuildsCountry(self, country): 
         """
-        Returns all guilds under a country
+        Returns all guilds under a country as tuples
         """
-    
+        self.run.execute(
+            """
+            SELECT name, description 
+            FROM Guild
+            WHERE country = ? 
+            """, (country)
+        )
+        guilds = self.run.fetchall()
 
-    def getAllGuildsState(state): 
-        """
-        Returns all guilds in a state. 
-        """
-    
-
-    def getAllGuildsCity(city): 
-        """
-        Returns all guilds under a city: 
-        """
+        return guilds
 
 
-    def newPost(username, guildName, text, creationDate, pictureID = -1): 
+    def getAllGuildsState(self, state): 
+        """
+        Returns all guilds in a state as tuples
+        """
+        self.run.execute(
+            """
+            SELECT name, description 
+            FROM Guild
+            WHERE state = ? 
+            """, (state)
+        )
+        guilds = self.run.fetchall()
+
+        return guilds
+
+
+    def getAllGuildsCity(self, city): 
+        """
+        Returns all guilds under a city as tuples
+        """
+        self.run.execute(
+            """
+            SELECT name, description 
+            FROM Guild
+            WHERE city = ? 
+            """, (city)
+        )
+        guilds = self.run.fetchall()
+
+        return guilds
+
+
+    def newPost(self, username, guildName, text, creationDate, pictureID = -1): 
         """
         Creates a new post in the Posts table. 
         """
+        self.run.execute("""
+            INSERT INTO Posts (username, )
+        """)
 
 
-    def likePost(postID): 
+    def likePost(self, postID): 
         """
         Allows you to like a post. 
         """
     
 
-    def getAllPostsUser(username): 
+    def getAllPostsUser(self, username): 
         """
         Returns all posts under a user. 
         """
     
 
-    def getAllPostsGuild(guildName): 
+    def getAllPostsGuild(self, guildName): 
         """
         Returns all posts under a guild. 
         """
 
 
-    def newConversation(user1, user2): 
+    def newConversation(self, user1, user2): 
         """
         Creates a conversation between two people. 
         """
 
 
-    def newMessage(conversationID, sender, receiver, date, message): 
+    def newMessage(self, conversationID, sender, receiver, date, message): 
         """
         Creates a new message within a conversation between two people. 
         """
+        
     
 
     
